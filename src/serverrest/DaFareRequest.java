7eca580f0c6e4870e2f565ca0f6a6b70462260cc
sideri-samuele <sideri.samuele@franchettisalviani.net>
2026-02-20 09:25:06 +0100

    da_fare_request_V1

--- src/serverrest/DaFareRequest.java
+++ src/serverrest/DaFareRequest.java
@@ -9,21 +9,58 @@ package serverrest;
  * @author delfo
  */
 public class DaFareRequest {
-    
+    private String giocata;
+    private String numero;
+    private boolean vittoria;
     
     // Costruttore vuoto necessario per GSON
     public DaFareRequest() {
     }
     
     // Costruttore con parametri  
-   
+    public DaFareRequest(String giocata, String numero, boolean vittoria) {
+        this.giocata = giocata;
+        this.numero = numero;
+        this.vittoria = vittoria;
+    }
     
     // Getter
-   
+    public String getGiocata() {
+        return giocata;
+    }
+
+    public void setGiocata(String giocata) {
+        this.giocata = giocata;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
     
     // Setter
-    
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public boolean isVittoria() {
+        return vittoria;
+    }
+
+    public void setVittoria(boolean vittoria) {
+        this.vittoria = vittoria;
+    }
 
     // ToString
+
+    @Override
+    public String toString() {
+        return "giocata " + giocata + " vittoria " + vittoria + " numero " + numero;
+    }
+    
+    
+
+    
+
+    
     
 }
